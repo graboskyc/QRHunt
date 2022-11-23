add1687450232fab21dab0901a66fc6e85eb322f
Chris Grabosky <chris@grabosky.net>
2022-11-23 09:06:09 -0500

    sort by points then youngest check in for tie breaker

--- AppServicesExport/functions/getLeaderboardGraph.js
+++ AppServicesExport/functions/getLeaderboardGraph.js
@@ -1,44 +1,40 @@
 // This function is the endpoint's request handler.
 exports = async function() {
 
-    var conn = context.services.get("mongodb-atlas").db("qrhunt").collection("checkin");
+  var conn = context.services.get("mongodb-atlas").db("qrhunt").collection("checkin");
+  
+  var pipeline = [
+    {$match: {
+       'location.eventcode': context.values.get("preFilter")
+      }}, {$addFields: {
+       un: {
+        $concat: [
+         '$name',
+         '_',
+         '$owner'
+        ]
+       },
+       time: {
+        $toDate: '$_id'
+       }
+      }}, {$group: {
+       _id: '$un',
+       points: {
+        $sum: '$location.points'
+       },
+       lowest: {
+        $min: '$time'
+       }
+      }}, {$sort: {
+       points: -1,
+       lowest: 1
+      }}, {$limit: 10}, {$project: {
+       _id: 0,
+       key: '$_id',
+       value: '$points'
+    }}];
     
-    var pipeline = [{
-     $match: {
-      'location.eventcode': context.values.get("preFilter")
-     }
-    }, {
-     $addFields: {
-      un: {
-       $concat: [
-        '$name',
-        '_',
-        '$owner'
-       ]
-      }
-     }
-    }, {
-     $group: {
-      _id: '$un',
-      points: {
-       $sum: '$location.points'
-      }
-     }
-    }, {
-     $sort: {
-      points: -1
-     }
-    }, {
-     $limit: 10
-    }, {
-      $project: {
-        _id:0,
-        key:"$_id",
-        value:"$points"
-      }
-    }];
-      
-    var docs = await conn.aggregate(pipeline).toArray();
+  var docs = await conn.aggregate(pipeline).toArray();
 
-    return docs;
+  return docs;
 };
